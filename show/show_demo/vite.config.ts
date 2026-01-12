29fb481343c8c0545de6822f26689910aea6a9db
heyjibo <1175632321@QQ.COM>
2026-01-12 10:46:12 +0800

    plus

--- show/show_demo/vite.config.ts
+++ show/show_demo/vite.config.ts
@@ -6,5 +6,13 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: { '@': path.resolve(__dirname, 'src') }
+  },
+    server: {
+    // 配置允许访问的主机列表
+    allowedHosts: [
+      'localhost', // 保留本地访问（可选，默认已允许）
+      '127.0.0.1', // 保留本地访问（可选，默认已允许）
+      '7866126c62f8.ngrok-free.app' // 添加localtunnel生成的公网域名
+    ]
   }
 })
